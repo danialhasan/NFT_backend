f761a289621e075c1d76ad6e38fc138adfdf5de5
Danial Hasan <danialhasan14@gmail.com>
2021-11-27 21:20:21 -0500

    added dynamic NFT generation

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -5,6 +5,8 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
+import {Base64} from "./libraries/Base64.sol";
+
 contract NFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds; // this is our Counter variable, keeps track of tokenIds.
@@ -13,19 +15,137 @@ contract NFT is ERC721URIStorage {
         console.log("This is my NFT contract!");
     }
 
+    // Arrays of word that will be randomly used to generate NFT words.
+    string[] sizeSet = [
+        "Huge",
+        "Big",
+        "Small",
+        "Tiny",
+        "Miniscule",
+        "UltraWide",
+        "Baby",
+        "Enormous",
+        "Micro"
+    ];
+    string[] colorSet = [
+        "Black",
+        "White",
+        "Beige",
+        "Red",
+        "Blue",
+        "Grey",
+        "Yellow",
+        "Brown",
+        "Green",
+        "Purple"
+    ];
+    string[] speciesSet = [
+        "Jaguar",
+        "Tiger",
+        "Lion",
+        "Cougar",
+        "Minx",
+        "Bobcat",
+        "Pussycat",
+        "Cheetah",
+        "Jungle Cat",
+        "Leopard",
+        "Cat"
+    ];
+
+    // base SVG string all our dynamically-created NFTs are based off of.
+    string baseSvg =
+        "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+    // Random Word Generator functions
+
+    function pickFirstRandomWord(uint256 tokenId)
+        private
+        returns (string memory)
+    {
+        uint256 rand = random(
+            string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId)))
+        );
+        rand = rand % sizeSet.length;
+        return sizeSet[rand];
+    }
+
+    function pickSecondRandomWord(uint256 tokenId)
+        private
+        returns (string memory)
+    {
+        uint256 rand = random(
+            string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId)))
+        );
+        rand = rand % colorSet.length;
+        return colorSet[rand];
+    }
+
+    function pickThirdRandomWord(uint256 tokenId)
+        private
+        returns (string memory)
+    {
+        uint256 rand = random(
+            string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId)))
+        );
+        rand = rand % speciesSet.length;
+        return speciesSet[rand];
+    }
+
+    function random(string memory input) internal pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(input)));
+    }
+
     // function for minting NFT to user
     function mintCatNFT() public {
         // get current tokenId (starts at 0). This keeps track of each individual NFT within the collection.
         uint256 newItemId = _tokenIds.current();
 
+        // get random words.
+        string memory firstWord = pickFirstRandomWord(newItemId);
+        string memory secondWord = pickSecondRandomWord(newItemId);
+        string memory thirdWord = pickThirdRandomWord(newItemId);
+        string memory combinedWord = string(
+            abi.encodePacked(firstWord, secondWord, thirdWord)
+        );
+
+        // concat all words into svg, then base64 encode it.
+        string memory finalSvg = string(
+            abi.encodePacked(baseSvg, combinedWord, "</text></svg>")
+        );
+        console.log("Final SVG:");
+
+        console.log("----------------");
+        console.log(finalSvg);
+        console.log("----------------");
+
+        // Get all the JSON metadata in place and base64 encode it.
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        // We set the title of our NFT as the generated word.
+                        combinedWord,
+                        '", "description": "A highly acclaimed collection of cats.", "image": "data:image/svg+xml;base64,',
+                        // We add data:image/svg+xml;base64 and then append our base64 encode our svg.
+                        Base64.encode(bytes(finalSvg)),
+                        '"}'
+                    )
+                )
+            )
+        );
+
+        // Just like before, we prepend data:application/json;base64, to our data.
+        string memory finalTokenUri = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+
         // Mint NFT to sender and give the NFT the current tokenId.
         _safeMint(msg.sender, newItemId);
 
         // set the data of the NFT with the specified newItemId.
-        _setTokenURI(
-            newItemId,
-            "data:application/json;base64,eyJuYW1lIjoiQmVsbHkiLCJkZXNjcmlwdGlvbiI6IlJlZCBub3NlLiIsImltYWdlIjoiZGF0YTppbWFnZS9zdmcreG1sO2Jhc2U2NCxQSE4yWnlCNGJXeHVjejBpYUhSMGNEb3ZMM2QzZHk1M015NXZjbWN2TWpBd01DOXpkbWNpSUhCeVpYTmxjblpsUVhOd1pXTjBVbUYwYVc4OUluaE5hVzVaVFdsdUlHMWxaWFFpSUhacFpYZENiM2c5SWpBZ01DQXpOVEFnTXpVd0lqNEtJQ0FnSUR4emRIbHNaVDR1WW1GelpTQjdJR1pwYkd3NklIZG9hWFJsT3lCbWIyNTBMV1poYldsc2VUb2djMlZ5YVdZN0lHWnZiblF0YzJsNlpUb2dNVFJ3ZURzZ2ZUd3ZjM1I1YkdVK0NpQWdJQ0E4Y21WamRDQjNhV1IwYUQwaU1UQXdKU0lnYUdWcFoyaDBQU0l4TURBbElpQm1hV3hzUFNKaWJHRmpheUlnTHo0S0lDQWdJRHgwWlhoMElIZzlJalV3SlNJZ2VUMGlOVEFsSWlCamJHRnpjejBpWW1GelpTSWdaRzl0YVc1aGJuUXRZbUZ6Wld4cGJtVTlJbTFwWkdSc1pTSWdkR1Y0ZEMxaGJtTm9iM0k5SW0xcFpHUnNaU0krUlhCcFkweHZjbVJJWVcxaWRYSm5aWEk4TDNSbGVIUStDand2YzNablBnPT0ifQ=="
-        );
+        _setTokenURI(newItemId, finalTokenUri);
         console.log("Minted NFT # %s to %s", newItemId, msg.sender);
         // Increment the counter to the ID of the next NFT to be minted.
         _tokenIds.increment();
